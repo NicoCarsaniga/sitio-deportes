2f25516ec4e2d953844af4b6183265ec13e3a91a
Hector Liceaga <lice2187@gmail.com>
2020-05-06 11:30:35 -0300

    cambios menores e indentación

--- views/spokon.view.php
+++ views/spokon.view.php
@@ -14,18 +14,18 @@ class SpokonView
         //var_dump($itemList);
         echo '<ul>';
         foreach ($itemList as $item) {
-            echo '<li>' . $item->torneo . ' </li>';
+            echo '<li><a href="tournament/' . $itemList->id_torneo . '">' . $item->torneo . '</a>';
         }
         echo '</ul>';
     }
-    
+
     public function showCategoryList($categories)
     {
-        
-        
+
+
         echo '<ul>';
         foreach ($categories as $category) {
-            echo '<li><a href="tournament/' . $category->id_deporte . '">' . $category->deporte . '</a>';
+            echo '<li><a href="sport/' . $category->id_deporte . '">' . $category->deporte . '</a>';
         }
         echo '</ul>';
     }
@@ -34,7 +34,7 @@ class SpokonView
     {
         echo '<h1> Bienvenidos a SPOKON</H1>';
         echo '<h3> El sitio para disfrutar de tus deportes favoritos </h3>';
-        echo '<h4>'. $tournamentById->deporte . '</h4>';
+        echo '<h4>' . $tournamentById->deporte . '</h4>';
         echo '<ul>';
         foreach ($itemListById as $item) {
             echo '<li>' . $item->torneo . ' </li>';
